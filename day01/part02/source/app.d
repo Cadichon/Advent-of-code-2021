9d753b5b88d7032bcea6aecb4538505cc69cac6b
Léo Labruyère <leo.labruyere@pm.me>
2021-12-02 23:10:37 +0100

    Added a second input files Moved all inputs in the day01 folder Added day01/part02

---
+++ day01/part02/source/app.d
@@ -0,0 +1,12 @@
+import std.stdio : writeln;
+import std.algorithm : map, each, sum, fold;
+import std.conv : to;
+import std.array : split;
+import std.range : slide;
+
+auto input = import("./input.txt").split.map!(to!uint);
+
+void main()
+{
+	writeln(input.slide(3).map!(sum).slide(2).fold!((acc, pair) => acc + (pair[1] > pair[0] ? 1 : 0))(0));
+}
